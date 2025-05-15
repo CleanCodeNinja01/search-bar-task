fb184fc5e2f2199334df0bd4b3c4b437b6ab075e
CleanCodeNinja01 <saleha.shujaat1@gmail.com>
2025-05-15 14:26:04 +0500

    fix: handle running server without testing compile error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
 
 // https://vite.dev/config/
 export default defineConfig({
